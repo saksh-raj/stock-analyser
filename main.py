97b09bb79ef61a6f41cca101fbfb25008a3ff4c4
Saksham Raj <sakraj.2006@gmail.com>
2025-09-24 05:53:18 +0530

    knf

--- main.py
+++ main.py
@@ -24,6 +24,40 @@ def get_sales_yearly(ticker, year = 0):
     Sales = yearly_profit_loss_account.loc[0]
     return Sales.iloc[year-2]
 
+def get_peer_comparision(ticker):
+    try:
+        peers = get_peer_names(ticker)
+        PE = []
+        ROCE = []
+        ROE = []
+        MARCAP = []
+        CMP = []
+        for peer in peers:
+            link = f"https://www.screener.in/company/{peer}"
+            response = requests.get(link)
+            response.raise_for_status()
+            soup = BS(response.text, 'html.parser')
+            numbers = soup.find_all('span', class_="number")
+            temp = numbers[0].get_text().replace(',','')
+            MARCAP.append(float(temp))
+            temp = numbers[1].get_text().replace(',','')
+            CMP.append(float(temp))
+            temp = numbers[4].get_text().replace(',','')
+            PE.append(float(temp))
+            temp = numbers[7].get_text().replace(',','')
+            ROCE.append(float(temp))
+            temp = numbers[8].get_text().replace(',','')
+            ROE.append(float(temp))
+        return PE, ROCE, ROE, MARCAP, CMP
+        
+    except HTTPError:
+        if response.status_code == 404 :
+            print("Name a valid ticker! 404")
+        else:
+            print(f"Some error occured! {response.status_code}")
+        quit()
+    
+
 def get_peer_names(ticker):
     try:
         link = f"https://www.screener.in/company/{ticker}"
@@ -31,14 +65,28 @@ def get_peer_names(ticker):
         response.raise_for_status()
         soup = BS(response.text, 'html.parser')
         randomNumber = soup.find(id = "peers").find('p').find_all('a')[-1]['href'].strip()
-        peerURL = f"https://www.screener.in/{randomNumber}"
+        link = f"https://www.screener.in/{randomNumber}"
+        response = requests.get(link)
+        response.raise_for_status()
+        soup = BS(response.text, 'html.parser')
+        del randomNumber
+        table = soup.find('table')
+        companiesTD = table.find_all('td')
+        list = []
+        for company in companiesTD:
+            anchor_tag = company.find('a')
+            if anchor_tag and anchor_tag.has_attr("href"):
+                anchor = anchor_tag["href"].strip()
+                anchor = anchor[:-13]
+                list.append(anchor[9:-1])
+        return list
         
     except HTTPError:
         if response.status_code == 404 :
             print("Name a valid ticker! 404")
         else:
             print(f"Some error occured! {response.status_code}")
-    quit()
+        quit()
 
 
 
@@ -56,21 +104,11 @@ except HTTPError:
 # # Most Recent FY -> Apr '24 - Mar'25
 # # Most Recent Quarter -> Jun '25
 
-print(get_peer_names(ticker))
+print(get_peer_comparision(ticker))
 
 # # print("All values are in Rs. Crores and these are standalone figures.")
 # tables = get_stock_data(ticker)
 # YearlyPL = get_yearly_profit_loss_account(tables)
-
-# #Peers
-# soup = BS(response.text, 'html.parser')
-# peerSec = soup.find(id = "peers")
-# peerSecP = peerSec.find('p')
-# peerSecPATags = peerSecP.find_all('a')
-# relevantATag = peerSecPATags[-1]["href"]
-# industryHREF = relevantATag.strip()
-# peerURL = "https://www.screener.in/"+industryHREF
-# print(peerURL)
 
 # #Sales Section
 # latest_Sales = get_sales_yearly(ticker)
